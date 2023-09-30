500d0df45877e74c587034af1c7f3f4599a155c1
Harish Sekar <harishrsekar@gmail.com>
2023-09-30 15:46:01 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./Carousel.css";
 
-function App() {
+const Carousel = () => {
+  const images = [
+    "https://via.placeholder.com/400x300?text=Image+1",
+    "https://via.placeholder.com/400x300?text=Image+2",
+    "https://via.placeholder.com/400x300?text=Image+3",
+    "https://via.placeholder.com/400x300?text=Image+4",
+  ];
+
+  const [currentImageIndex, setCurrentImageIndex] = useState(0);
+
+  const nextImage = () => {
+    setCurrentImageIndex((prevIndex) => (prevIndex + 1) % images.length);
+  };
+
+  const prevImage = () => {
+    setCurrentImageIndex((prevIndex) =>
+      prevIndex === 0 ? images.length - 1 : prevIndex - 1
+    );
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="carousel-container centered">
+      <h1 className="carousel-heading">Gallery</h1>
+      <div className="carousel-wrapper">
+        <button className="carousel-button round left" onClick={prevImage}>
+          &#9664;
+        </button>
+        <img
+          src={images[currentImageIndex]}
+          alt={`Image ${currentImageIndex + 1}`}
+          className="carousel-image"
+        />
+        <button className="carousel-button round right" onClick={nextImage}>
+          &#9654; {/* Right arrow */}
+        </button>
+      </div>
     </div>
   );
-}
+};
 
-export default App;
+export default Carousel;
